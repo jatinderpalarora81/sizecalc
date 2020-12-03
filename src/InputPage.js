da4b7a24f66fa978360b1aee88331374605c9d26
Jatin <jatinderpalarora81@gmail.com>
2020-12-03 15:18:47 +0530

    added size calc

--- src/InputPage.js
+++ src/InputPage.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import {Button, Col, Container, Form, FormControl, Image, InputGroup, Row} from "react-bootstrap";
-import Jumbotron from './pic.JPG';
+import pic from './pic.JPG';
+import {calculateSize} from "./util/sizecalcutil";
 
 export class InputPage extends React.Component{
 
@@ -27,12 +28,14 @@ export class InputPage extends React.Component{
                     <div style={{width:'800px', height:'900px'}}>
                         <Form>
                             <Form.Group controlId="n1">
+                                <Form.Label>Name</Form.Label>
                                 <Form.Control type="text" placeholder="Enter Name"
                                 onChange={e=>{this.values.name= e.target.value}}
                                 />
                             </Form.Group>
 
                             <Form.Group controlId="a1">
+                                <Form.Label>Age</Form.Label>
                                 <Form.Control type="number" placeholder="Your Age" onChange={e=>{this.values.age= e.target.value}}
                                 />
                                 <Form.Text className="text-muted">
@@ -41,32 +44,37 @@ export class InputPage extends React.Component{
                             </Form.Group>
 
                             <Form.Group controlId="h1">
+                                <Form.Label>Height</Form.Label>
                                 <Form.Control type="number" placeholder="Enter Height (in inches)" onChange={e=>{this.values.height= e.target.value}}
                                 />
                             </Form.Group>
 
                             <Form.Group controlId="b1">
+                                <Form.Label>Bust Size:</Form.Label>
                                 <Form.Control type="number" placeholder="Enter bust (in inches)" onChange={e=>{this.values.bust= e.target.value}}
                                 />
                             </Form.Group>
 
                             <Form.Group controlId="w1">
+                                <Form.Label>Drop Waist Size: </Form.Label>
                                 <Form.Control type="number" placeholder="Enter drop waist value (in inches)" onChange={e=>{this.values.waist= e.target.value}}
                                 />
                             </Form.Group>
 
                             <Form.Group controlId="hi1">
-                                <Form.Control type="number" placeholder="Enter drop hips size (in inches)"   onChange={e=>{this.values.hips= e.target.value}}
+                                <Form.Label>Hip size: </Form.Label>
+                                <Form.Control type="number" placeholder="Enter  hips size (in inches)"   onChange={e=>{this.values.hips= e.target.value}}
                                 />
                             </Form.Group>
-                            <Button variant="primary" type="button" onClick={()=>alert("CLICKED"+this.values.name)}>
+                            <Button variant="primary" type="button" onClick={()=>alert("You reccomended size is : "+calculateSize(this.values))}>
                                 Submit
                             </Button>
                         </Form>
                     </div>
                 </Col>
                 <Col>
-                    <img style={{height:'auto',width:'100%'}} src={ Jumbotron }/>
+                    {/*<img src={Jumbotron} className="App-logo" alt="logo" />*/}
+                    <img style={{height:'auto',width:'100%'}} src={ pic }/>
                 </Col>
 
             </Row>
